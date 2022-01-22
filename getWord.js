6a6f24d50b0bd356fa10bc56a1bee00f8a721c45
MidgetDude <61662276+MidgetDude@users.noreply.github.com>
2022-01-22 16:06:17 +0200

    Fixed bug in random word getting

--- getWord.js
+++ getWord.js
@@ -1,15 +1,13 @@
-const { randomInt } = require("crypto");
 const fs = require("fs");
 
 let raw = fs.readFileSync('words.json');
 let words = JSON.parse(raw);
 
-var length = -1;
+var length = 0;
 for(var word in words) if(words.hasOwnProperty(word)) length++;
 
-var randInt = getRandomInt(length);
-
-console.log(word[randInt]);
+var randInt = getRandomInt(length - 1);
+console.log(words[randInt]);
 
 function getRandomInt(max){
     return Math.floor(Math.random()*max);
